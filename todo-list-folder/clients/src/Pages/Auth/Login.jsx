29a006ddfb5b17ffb2a15a9274c684df66a67e11
huy <huy.ma@esbt.vn>
2025-08-27 11:00:50 +0700

    todo

--- todo-list-folder/clients/src/Pages/Auth/Login.jsx
+++ todo-list-folder/clients/src/Pages/Auth/Login.jsx
@@ -1,18 +1,55 @@
-import {FaUserCircle} from 'react-icons/fa'
-import "./Login.css"
+import { FaUserCircle } from "react-icons/fa";
+import "./Login.css";
+import { useState } from "react";
+import AuthServices from "../../service/authServices";
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassWord] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data = {
+      userName,
+      password,
+    };
+    const login = await AuthServices.loginUser(data);
+    console.log(login);
+    if (login.status === 200) {
+      navigate("/to-do");
+    } else {
+      alert("Đăng nhập thất bại!");
+    }
+  };
   return (
-    <div className='container'>
-      <div className='login-box'>
-        <FaUserCircle className='icon'/>
+    <div className="container">
+      <div className="login-box">
+        <FaUserCircle className="icon" />
         <h2>Login</h2>
-        <input type="text" placeholder='Username' defaultValue="" />
-        <input type='text' placeholder='Password'/>
-        <p className='register-text'>
-          New Users? <a href='/register'>Register</a>
+        <input
+          type="text"
+          placeholder="Username"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassWord(e.target.value)}
+        />
+        <p className="register-text">
+          New Users? <a href="/register">Register</a>
         </p>
-        <button className='login-btn'>Login</button>
+        <button
+          className="login-btn"
+          disabled={!userName || !password}
+          onClick={(e) => handleSubmit(e)}
+        >
+          Login
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
--- todo-list-folder/clients/src/Pages/Auth/Register.jsx
+++ todo-list-folder/clients/src/Pages/Auth/Register.jsx
@@ -1,31 +1,79 @@
-import React from 'react'
-import { FaEyeSlash, FaUserCircle } from 'react-icons/fa'
-import "./Register.css"
+import React, { useState } from "react";
+import { FaEyeSlash, FaUserCircle } from "react-icons/fa";
+import "./Register.css";
+import { useNavigate } from "react-router-dom";
+import AuthServices from "../../service/authServices";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const pushNav = useNavigate();
+  const handleRegister = async () => {
+    if(!firstName || !lastName || !userName || !password) {
+      alert("please enter input")
+    }
+    const data = {
+      firstName, lastName, userName, password
+    }
+   
+      const res = AuthServices.registerUser(data);
+      if(!res.message){
+        pushNav("/login")
+      } else {
+        alert(res.message)
+      }
+      
+    
+    
+  };
+
   return (
-    <div className='container'>
-      <div className='auth-box'>
-        <FaUserCircle className='icon'/>
+    <div className="container">
+      <div className="auth-box">
+        <FaUserCircle className="icon" />
         <h2>Register</h2>
-        <div className='name-row'> 
-          <input type='text' placeholder='First Name'/>
-          <input type='text' placeholder='Last Name'/>
+        <div className="name-row">
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </div>
 
-        <input type='text' placeholder='Username' className='full-input'/>
-        <div className='input-with-icon'>
+        <input
+          type="text"
+          placeholder="Username"
+          className="full-input"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <div className="input-with-icon">
           <input
-          type='password'
-          placeholder='Password'
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <FaEyeSlash className='trail-icon' aria-hidden/>
+          <FaEyeSlash className="trail-icon" aria-hidden />
         </div>
-        <p className='muted'>Existing User? <a href='/login'>Login</a></p>
-        <button className='primary-btn'>Register</button>
+        <p className="muted">
+          Existing User? <a href="/login">Login</a>
+        </p>
+        <button className="primary-btn" onClick={handleRegister}>
+          Register
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
